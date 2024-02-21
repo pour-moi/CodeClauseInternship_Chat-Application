c9b65044864b30a13857a117f04b139e4dd3348c
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-02-21 10:43:15 +0300

    function to join specific room removed

--- server/index.js
+++ server/index.js
@@ -10,17 +10,16 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`user connected: ${socket.id}`);
 
-  socket.on("join_room", (roomName) => {
-    socket.join(roomName);
-  });
+  socket.on("send_message", (data) => io.emit("receive_message", data));
+});
 
-  socket.on("send_message", (data) => {
-    io.to(data.room).emit("receive_message", data);
-  });
+server.listen(5174, () => {
+  console.log("server running");
 });
